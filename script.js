deb74c0901dd44646081e13961e89ebf663e74a8
Fafooo <62049344+fafo-hub@users.noreply.github.com>
2020-11-03 14:50:44 -0800

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(`http://worldtimeapi.org/api/ip`).then(response => response.json()).then(data => {
+    document.querySelector('.time-zone').innerText = data.timezone;
+    document.querySelector('#date-time').value = new Date(data.datetime).toDateString() + ',' + new Date(data.datetime).toTimeString();
+    document.querySelector('#client-api').value = data.client_ip;
+    document.querySelector('#t-z').value = data.timezone;
+    })
+})
+
+function evolve() {
+    var e = document.querySelector('.continent')
+    var selfrst = e.value
+    console.log(selfrst);
+    
+    fetch(`http://worldtimeapi.org/api/timezone/${selfrst}`).then(response => response.json()).then(data => {
+        let theTimeZoneList =  data.join('<br/>')
+        document.querySelector('.display-time-zone').innerHTML = theTimeZoneList;
+    })
+}
